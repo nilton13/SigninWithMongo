beeea60a49e2e5ce79ebbb31331f46a1bf9aa074
nilton <nfdev@pop-os.localdomain>
2023-01-01 10:39:54 -0300

    Authentication using NestJS,Passport, Jwt and MongoDB

---
+++ src/users/users.service.ts
@@ -0,0 +1,56 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
+import { Model } from 'mongoose';
+import { AuthService } from 'src/auth/auth.service';
+import { SigninDTO } from './dto/signin.dto';
+import { SignupDTO } from './dto/signup.dto';
+import { User } from './models/users.model';
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectModel('User')
+        private readonly usersModel: Model<User>,
+        private readonly authService: AuthService
+    ){}
+
+    public async signup(signupDto: SignupDTO): Promise<User>{
+        const user = new this.usersModel(signupDto);
+        return user.save();
+    }
+
+    public async sigin(siginDto: SigninDTO): Promise<{ name: string; jwtToken: string; email: string; }>{
+        const user = await this.findByEmail(siginDto.email);
+        const match = await this.checkPassword(siginDto.password, user);
+        if(!match){
+            throw new NotFoundException('Invalid Credentials');
+        }
+
+        const jwtToken = await this.authService.createAccessToken(user._id);
+
+        return { name: user.name, jwtToken,email: user.email }
+    }
+
+    public async findAll(): Promise<User[]>{
+        return this.usersModel.find();
+    }
+
+    public async findByEmail(email: string): Promise<User>{
+        const user = await this.usersModel.findOne({ email });
+        if(!user){
+            throw new NotFoundException("Email not found.");
+        }
+
+        return user;
+    }    
+
+    private async checkPassword(password: string, user: User): Promise<boolean>{
+        const match = await bcrypt.compare(password, user.password);
+        if(!match){
+            throw new NotFoundException('Password not found.');
+        }
+
+        return match;
+    }
+}
